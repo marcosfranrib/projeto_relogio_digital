5b7b466d0a831f97eda3e044ffb3787631529a16
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-25 18:44:55 -0300

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+
+setInterval(function tempo(){
+   
+    let hora = new Date()
+    let h = hora.getHours()
+    let m = hora.getMinutes()
+    let s = hora.getSeconds()
+
+    let horario = document.getElementById('hora')
+    let minuto = document.getElementById('minuto')
+    let segundo = document.getElementById('segundo')
+
+    if(h < 10){h = '0' + h}
+    if(m < 10){m = '0' + m}
+    if(s < 10){s = '0' + s}
+
+    horario.innerHTML = h
+    minuto.innerHTML = m
+    segundo.innerHTML = s
+
+
+})
+
+
+
