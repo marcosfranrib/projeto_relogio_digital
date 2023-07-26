59d51c7e3b8d347dd2d0d4e5fcea2e4049e8e5dd
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-26 01:06:27 -0300

    .

--- script.js
+++ script.js
@@ -21,17 +21,34 @@ setInterval(function tempo(){
    
 })
 
-function tema(){
-    let body = document.body
-    let checar = document.getElementsByName('checar')
-   
-    if(checar[0].checked){
-     body.style.backgroundColor = '#021d2e'
+function mudar(){
+    let check = document.getElementById('chk')
+    let body = document.body.style
+    let containerR = document.getElementById('container-relogio')
+    let div = document.getElementsByClassName('padrao-div')
 
-    }else{
+  
 
-        body.style.backgroundColor = '#fff2f2'
+    if(check.checked == false){
+        body.backgroundColor = 'black'
+        containerR.style.backgroundColor = '#021d2e'
+        for(let c=0; c <= div.length; c++){
+            div[c].style.backgroundColor = 'white'
+    
+        }
+        
+    }else if(check.checked == true){
+        body.backgroundColor = '#fff2f2'
+        containerR.style.backgroundColor = ''
+        for(let c=0; c <= div.length; c++){
+            div[c].style.backgroundColor = 'white'
+    
+        }
     }
+
+    
+
+    
 
 }
 
